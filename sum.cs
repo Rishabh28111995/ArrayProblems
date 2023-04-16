a62e9b2a39b57390da5f87d93d2c9722db07e65b
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-17 02:30:39 +0530

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,24 @@
+﻿namespace Array4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the array: ");
+        int length = int.Parse(Console.ReadLine());
+        int[] arr = new int[length];
+        int sum = 0;
+
+        Console.WriteLine("Enter {0} elements in array: ", length);
+        for(int i = 0; i < length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            sum += arr[i];
+        }
+        Console.WriteLine("sum is = {0}", sum);
+    }
+}
+
