ec8976030cd04c951ac85a2dde3e4812c5c18046
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-17 02:07:54 +0530

    Add files via upload

---
+++ Negative.cs
@@ -0,0 +1,26 @@
+﻿namespace Array3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the array: ");
+        int length = int.Parse(Console.ReadLine());
+        int[] arr = new int[length];
+        Console.WriteLine("Enter {0} elements in array", length);
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nNegative elements age: ");
+        for (int i = 0; i < length; i++)
+        {
+            if (arr[i] < 0)
+            {
+                Console.WriteLine("{0}\t", arr[i]);
+            }
+        }
+
+    }
+}
+
