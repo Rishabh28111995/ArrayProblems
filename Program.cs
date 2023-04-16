b0f612f2e95a5f1d16d62b382dbf5c804fcf7841
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-16 22:32:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Array1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter size of array: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] arr = new int[size];
+        int max1, max2;
+
+        Console.WriteLine("Enter {0} elements in the array:", size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+        max1 = max2 = arr[0];
+
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] > max1)
+            {
+                max2 = max1;
+                max1 = arr[i];
+            }
+            else if (arr[i] > max2 && arr[i] < max1)
+            {
+                
+                max2 = arr[i];
+            }
+        }
+
+        
+        Console.Write("Second Highest = {0}", max2);
+        Console.ReadLine();
+    }
+}
+
+  
+
+
